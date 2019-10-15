9abc27ebc6fc7bf0c38aabe78b0df51f581f8d29
Matthew Blagg <matthewscottblagg@gmail.com>
2019-10-15 15:57:54 -0500

    fast food started

---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+const restaurant = {
+    name: "Bob's Burgers",
+    orders: ["cheese burger", "burger", "fries", "shake", "coke"],
+    placeOrder: function (meal) {
+    }
+}
+
+const chickenComboMeal = {
+
+}
+
+// Place an order
+restaurant.placeOrder()
+
+// Invoke the function to return the list of all orders
+
+// Output all orders to the console using console.table()
