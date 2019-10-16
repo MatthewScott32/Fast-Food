b009e8c0eec90292d11fb1a092baa0c5234ac64f
Matthew Blagg <matthewscottblagg@gmail.com>
2019-10-16 13:19:38 -0500

    finished fast food

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,33 @@
 const restaurant = {
     name: "Bob's Burgers",
-    orders:[["ruben", "chicken", "club"], ["fries", "none"], ["small", "medium", "large"]],
+    orders:[],
     placeOrder: function (meal) {
+        this.orders.push(meal);
+        
+    },
+    getOrders: function() {
+        return(this.orders);
     }
 }
-
+     
 const chickenComboMeal = {
+    sandwhich: "chicken",
+    fries: "true",
+    drink: "large"
 
+}
+
+const cheeseBurgerComboMeal = {
+    sandwhich: "burger",
+    fries: "false",
+    drink: "small"
 }
 
 // Place an order
-restaurant.placeOrder()
-
+restaurant.placeOrder(chickenComboMeal)
+console.log(restaurant.orders);
+restaurant.placeOrder(cheeseBurgerComboMeal)
+console.log(restaurant.orders);
 // Invoke the function to return the list of all orders
 
 // Output all orders to the console using console.table()
